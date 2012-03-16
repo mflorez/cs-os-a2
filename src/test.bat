dc9ed24fb07754882740bbafd58a1ce20ccf0c6c
Moises Florez <mflorez@gmail.com>
2012-03-16 18:38:30 -0400

    Minor stable update

--- src/test.bat
+++ src/test.bat
@@ -9,11 +9,11 @@ javac -classpath pa2-120312.jar;. *.java
 :: java -ea -cp pa2-120312.jar;. main OS pa1-tree.dsk
 :: java -ea -cp pa2-120312.jar;. main OS pa1-ytree.dsk
 
-:: java -ea -cp pa2-120312.jar;. main OS pa2-diskio.dsk
-:: java -jar pdb-*.jar results.dsk 511
+java -ea -cp pa2-120312.jar;. main OS pa2-diskio.dsk
+java -jar pdb-*.jar results.dsk 511
 
-java -ea -cp pa2-120312.jar;. main OS pa2-3dio.dsk
-java -jar pdb-*.jar results.dsk 509 510 511
+:: java -ea -cp pa2-120312.jar;. main OS pa2-3dio.dsk
+:: java -jar pdb-*.jar results.dsk 509 510 511
 
 :: java -ea -cp pa2-120312.jar;. main OS pa2-shared.dsk
 :: java -ea -cp pa2-120312.jar;. main OS pa2-dio-checked.dsk
